85372b2ccf6d23ca47d5b8cbe270787e21a2b210
Paige Hetherington <herbmama@gmail.com>
2016-02-12 15:57:30 -0500

    refactored into separate methods and added comments

--- src/Countries.java
+++ src/Countries.java
@@ -10,36 +10,21 @@ import java.util.Scanner;
 
 public class Countries {
 
-    public static HashMap<Character, ArrayList<Country>> countryMap = new HashMap<>();
+    public static HashMap<Character, ArrayList<Country>> countryMap = new HashMap<>(); //created HM key = abbrev and AL of countries
 
-    public static void main(String[] args) throws Exception {
-       readFile();
+    public static void main(String[] args) throws Exception { //main method
+       readFile(); //calling read file method
 
-        System.out.println("Please type the first letter of the country.");
-        Scanner scanner = new Scanner(System.in);
-        String letter = scanner.nextLine();
-        if (letter.length() != 1) {
-            throw new Exception("Please enter only one letter");
-        }
-        if (letter.equalsIgnoreCase("x")) {
-            throw new Exception("There are no countries listed under that letter. Please try again");
 
-        }
 
-        char firstLetter = letter.charAt(0);// first Letter comes from the string letter the user typed in
-        ArrayList<Country> subsetCountries = countryMap.get(firstLetter);
-        System.out.println(subsetCountries);
-        String fileName = firstLetter + "_Countries.txt";
-        String fileContent = new String();
-        for (Country c : subsetCountries) {
-            fileContent = fileContent + c.name + " " + c.abbreviation + "\n";
-        }
-        saveFile(fileName, fileContent);
-        saveJson(firstLetter);
+        char firstLetter = receiveSingleLetter();// create variable.firstLetter comes from the string letter the user typed in
+
+        saveFile(firstLetter); //call save method
+        saveJson(firstLetter); //call save json method
     }
 
 
-    public static void saveJson(Character input) throws IOException { //create save method
+    public static void saveJson(char input) throws IOException { //create save method. start with empty and see what need to pass in
         JsonSerializer s = new JsonSerializer(); //new object
         String json = s.include("*").serialize(countryMap.get(input)); //serialize into string -- (include("*") includes array list)
 
@@ -48,14 +33,21 @@ public class Countries {
         fw.write(json);
         fw.close();
     }
-    static void saveFile(String fileName, String fileContent) throws IOException {
+    static void saveFile(char firstLetter) throws IOException { //define save file method
+        ArrayList<Country> subsetCountries = countryMap.get(firstLetter); //list of countries based on firstLetter
+        System.out.println(subsetCountries); //prints arrayList
+        String fileName = firstLetter + "_Countries.txt"; //string object to name file
+        String fileContent = new String(); //empty string object for file content
+        for (Country c : subsetCountries) { //for loop fileContent; loop subsetCountries AL. local variable c
+            fileContent = fileContent + c.name + " " + c.abbreviation + "\n"; //put this in fileContent
+        }
         File f = new File(fileName);
         FileWriter fw = new FileWriter(f);
         fw.write(fileContent);
         fw.close();
 
     }
-    static void readFile() throws FileNotFoundException {
+    static void readFile() throws FileNotFoundException {  //define readFile method
         ArrayList<Country> allCountries = new ArrayList<>();
 
         File f = new File("countries.txt");
@@ -68,15 +60,30 @@ public class Countries {
         }
 
 
-        for (Country c : allCountries) {
-            char firstLetter = c.name.charAt(0);
-            countryMap.put(firstLetter, new ArrayList<Country>());
+        for (Country c : allCountries) { // for temp variable c in array list AL
+            char firstLetter = c.name.charAt(0); //char variable firstLetter  = the country's name's first character
+            countryMap.put(firstLetter, new ArrayList<Country>()); // add to the hashMap
         }
 
         for (Country c : allCountries) {
             char firstLetter = c.name.charAt(0);
             countryMap.get(firstLetter).add(c);
         }
+    }
+
+    static char receiveSingleLetter () throws Exception {
+        System.out.println("Please type the first letter of the country."); //asks user for input
+        Scanner scanner = new Scanner(System.in); //scanner receives input
+        String letter = scanner.nextLine(); //create string object for user's input
+        if (letter.length() != 1) { //checking to see user entered only 1 letter, if not throw exception
+            throw new Exception("Please enter only one letter");
+        }
+        if (letter.equalsIgnoreCase("x")) { //checking to see if they entered x which has no countries listed
+            throw new Exception("There are no countries listed under that letter. Please try again");
+
+
+        }
+        return letter.charAt(0);
     }
 
 
--- src/Country.java
+++ src/Country.java
@@ -1,16 +1,16 @@
 /**
  * Created by vajrayogini on 2/11/16.
  */
-public class Country {
+public class Country { //created class Country with 2 strings
     String name;
     String abbreviation;
 
-    public Country(String name, String abbreviation) {
+    public Country(String name, String abbreviation) { //created a constructor
         this.name = name;
         this.abbreviation = abbreviation;
     }
 
-    public String getName() {
+    public String getName() { //created getters so could encode output as json
         return name;
     }
 
